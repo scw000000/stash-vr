fe0f3c2fd344dfe6611af007c7c2030fc45c878e
scw000000 <scw000000@gmail.com>
2026-05-07 06:11:22 -0500

    config: add env vars for auto-section generation

--- internal/config/application.go
+++ internal/config/application.go
@@ -20,6 +20,18 @@ const (
 	envKeyExcludeSortName    = "EXCLUDE_SORT_NAME"
 	envKeyUserConfigPath     = "CONFIG_PATH"
 	envKeyGenerateSummaryIds = "GENERATE_SUMMARY_IDS"
+	envKeyAutoSectionsPerformers = "AUTO_SECTIONS_PERFORMERS"
+	envKeyMinScenesPerPerformer  = "MIN_SCENES_PER_PERFORMER"
+	envKeyMaxPerformerSections   = "MAX_PERFORMER_SECTIONS"
+	envKeyAutoSectionsTags       = "AUTO_SECTIONS_TAGS"
+	envKeyTopNTags               = "TOP_N_TAGS"
+	envKeyAutoSectionsAggregates = "AUTO_SECTIONS_AGGREGATES"
+	envKeyAggregateRecentAdded   = "AGGREGATE_RECENT_ADDED"
+	envKeyAggregateRecentPlayed  = "AGGREGATE_RECENT_PLAYED"
+	envKeyAggregateHighlyRated   = "AGGREGATE_HIGHLY_RATED"
+	envKeyAggregateUnwatched     = "AGGREGATE_UNWATCHED"
+	envKeyHighlyRatedThreshold   = "HIGHLY_RATED_THRESHOLD"
+	envKeyAggregateLimit         = "AGGREGATE_LIMIT"
 )
 
 type ApplicationConfig struct {
@@ -35,6 +47,18 @@ type ApplicationConfig struct {
 	ExcludeSortName    string
 	ConfigPath         string
 	GenerateSummaryIds bool
+	AutoSectionsPerformers bool
+	MinScenesPerPerformer  int
+	MaxPerformerSections   int
+	AutoSectionsTags       bool
+	TopNTags               int
+	AutoSectionsAggregates bool
+	AggregateRecentAdded   bool
+	AggregateRecentPlayed  bool
+	AggregateHighlyRated   bool
+	AggregateUnwatched     bool
+	HighlyRatedThreshold   int
+	AggregateLimit         int
 }
 
 var applicationConfig ApplicationConfig
@@ -76,6 +100,42 @@ func Init() {
 	pflag.String(envKeyGenerateSummaryIds, "", "Generate summary ids for categorized tags")
 	_ = viper.BindPFlag(envKeyGenerateSummaryIds, pflag.Lookup(envKeyGenerateSummaryIds))
 
+	pflag.Bool(envKeyAutoSectionsPerformers, false, "Generate one HereSphere section per performer above MIN_SCENES_PER_PERFORMER")
+	_ = viper.BindPFlag(envKeyAutoSectionsPerformers, pflag.Lookup(envKeyAutoSectionsPerformers))
+
+	pflag.Int(envKeyMinScenesPerPerformer, 5, "Minimum scene count for a performer to get an auto-section")
+	_ = viper.BindPFlag(envKeyMinScenesPerPerformer, pflag.Lookup(envKeyMinScenesPerPerformer))
+
+	pflag.Int(envKeyMaxPerformerSections, 50, "Cap on per-performer auto-sections (ranked by scene_count desc)")
+	_ = viper.BindPFlag(envKeyMaxPerformerSections, pflag.Lookup(envKeyMaxPerformerSections))
+
+	pflag.Bool(envKeyAutoSectionsTags, false, "Generate one HereSphere section per tag in the top TOP_N_TAGS by scene_count")
+	_ = viper.BindPFlag(envKeyAutoSectionsTags, pflag.Lookup(envKeyAutoSectionsTags))
+
+	pflag.Int(envKeyTopNTags, 20, "Number of tag auto-sections")
+	_ = viper.BindPFlag(envKeyTopNTags, pflag.Lookup(envKeyTopNTags))
+
+	pflag.Bool(envKeyAutoSectionsAggregates, false, "Generate aggregate sections (Recently Added/Played/Highly Rated/Unwatched)")
+	_ = viper.BindPFlag(envKeyAutoSectionsAggregates, pflag.Lookup(envKeyAutoSectionsAggregates))
+
+	pflag.Bool(envKeyAggregateRecentAdded, true, "Sub-toggle for the Recently Added aggregate section")
+	_ = viper.BindPFlag(envKeyAggregateRecentAdded, pflag.Lookup(envKeyAggregateRecentAdded))
+
+	pflag.Bool(envKeyAggregateRecentPlayed, true, "Sub-toggle for the Recently Played aggregate section")
+	_ = viper.BindPFlag(envKeyAggregateRecentPlayed, pflag.Lookup(envKeyAggregateRecentPlayed))
+
+	pflag.Bool(envKeyAggregateHighlyRated, true, "Sub-toggle for the Highly Rated aggregate section")
+	_ = viper.BindPFlag(envKeyAggregateHighlyRated, pflag.Lookup(envKeyAggregateHighlyRated))
+
+	pflag.Bool(envKeyAggregateUnwatched, true, "Sub-toggle for the Unwatched aggregate section")
+	_ = viper.BindPFlag(envKeyAggregateUnwatched, pflag.Lookup(envKeyAggregateUnwatched))
+
+	pflag.Int(envKeyHighlyRatedThreshold, 80, "Stash 0-100 rating; scenes >= threshold qualify for Highly Rated")
+	_ = viper.BindPFlag(envKeyHighlyRatedThreshold, pflag.Lookup(envKeyHighlyRatedThreshold))
+
+	pflag.Int(envKeyAggregateLimit, 100, "Per-aggregate max scene count")
+	_ = viper.BindPFlag(envKeyAggregateLimit, pflag.Lookup(envKeyAggregateLimit))
+
 	pflag.BoolP("help", "h", false, "Display usage information")
 	_ = viper.BindPFlag("help", pflag.Lookup("help"))
 
@@ -100,6 +160,18 @@ func Init() {
 	applicationConfig.ExcludeSortName = viper.GetString(envKeyExcludeSortName)
 	applicationConfig.ConfigPath = viper.GetString(envKeyUserConfigPath)
 	applicationConfig.GenerateSummaryIds = viper.GetBool(envKeyGenerateSummaryIds)
+	applicationConfig.AutoSectionsPerformers = viper.GetBool(envKeyAutoSectionsPerformers)
+	applicationConfig.MinScenesPerPerformer = viper.GetInt(envKeyMinScenesPerPerformer)
+	applicationConfig.MaxPerformerSections = viper.GetInt(envKeyMaxPerformerSections)
+	applicationConfig.AutoSectionsTags = viper.GetBool(envKeyAutoSectionsTags)
+	applicationConfig.TopNTags = viper.GetInt(envKeyTopNTags)
+	applicationConfig.AutoSectionsAggregates = viper.GetBool(envKeyAutoSectionsAggregates)
+	applicationConfig.AggregateRecentAdded = viper.GetBool(envKeyAggregateRecentAdded)
+	applicationConfig.AggregateRecentPlayed = viper.GetBool(envKeyAggregateRecentPlayed)
+	applicationConfig.AggregateHighlyRated = viper.GetBool(envKeyAggregateHighlyRated)
+	applicationConfig.AggregateUnwatched = viper.GetBool(envKeyAggregateUnwatched)
+	applicationConfig.HighlyRatedThreshold = viper.GetInt(envKeyHighlyRatedThreshold)
+	applicationConfig.AggregateLimit = viper.GetInt(envKeyAggregateLimit)
 
 }
 
